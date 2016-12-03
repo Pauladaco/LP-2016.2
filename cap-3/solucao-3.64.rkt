22f8647e73d3b752b8326a1c1c84fcdc7c073977
Paula Bruna Daco <paulabrunadaco@gmail.com>
2016-12-03 15:28:34 -0200

    Delete solucao-3.64.rkt

--- cap-3/solucao-3.64.rkt
+++
@@ -1,5 +0,0 @@
-#lang racket
-(define (stream-limit stream tolerance)
-  (if (< (abs (- (stream-ref stream 1) (stream-ref stream 0))) tolerance)
-      (stream-ref stream 1)
-      (stream-limit (stream-cdr stream) tolerance)))

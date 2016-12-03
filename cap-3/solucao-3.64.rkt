51b2820f68ae81741955dab581902f5e04a7daf8
Paula Bruna Daco <paulabrunadaco@gmail.com>
2016-12-03 15:27:44 -0200

    Add files via upload

---
+++ cap-3/solucao-3.64.rkt
@@ -0,0 +1,5 @@
+#lang racket
+(define (stream-limit stream tolerance)
+  (if (< (abs (- (stream-ref stream 1) (stream-ref stream 0))) tolerance)
+      (stream-ref stream 1)
+      (stream-limit (stream-cdr stream) tolerance)))
